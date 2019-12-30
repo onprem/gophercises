1bc1b9c9b648e03a2c0fee797b1e564f06eb3336
Prem Kumar <prmsrswt@gmail.com>
2019-12-31 02:52:21 +0530

    quiz: add solution for part 2

--- quiz/quiz.go
+++ quiz/quiz.go
@@ -5,12 +5,37 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"time"
 )
 
 var csvPath string
+var limit int
 
 func init() {
 	flag.StringVar(&csvPath, "csv", "problems.csv", `a csv file in the format of 'questions,answers'`)
+	flag.IntVar(&limit, "limit", 30, "time limit for the quiz in seconds")
+}
+
+func askQuestions(questions [][]string, correctAnswers *int, exit chan bool) {
+	var ans string
+	for i, row := range questions {
+		fmt.Printf("Question #%d: %s = ", i+1, row[0])
+		fmt.Scanln(&ans)
+
+		if ans == row[1] {
+			*correctAnswers++
+		}
+	}
+	exit <- true
+}
+
+func timer(start chan bool, exit chan bool) {
+	fmt.Printf("Time limit for the quiz: %ds\nPress enter to start the quiz. ", limit)
+	fmt.Scanln()
+	start <- true
+
+	time.Sleep(time.Duration(limit) * time.Second)
+	exit <- true
 }
 
 func main() {
@@ -30,17 +55,15 @@ func main() {
 		return
 	}
 
-	var (
-		ans            string
-		correctAnswers int
-	)
-	for i, row := range questions {
-		fmt.Printf("Question #%d: %s = ", i, row[0])
-		fmt.Scanln(&ans)
+	var correctAnswers int
+	exit := make(chan bool)
+	start := make(chan bool)
 
-		if ans == row[1] {
-			correctAnswers++
-		}
-	}
-	fmt.Printf("You scored %d out of %d.\n", correctAnswers, len(questions))
+	go timer(start, exit)
+
+	<-start
+	go askQuestions(questions, &correctAnswers, exit)
+
+	<-exit
+	fmt.Printf("\nYou scored %d out of %d.\n", correctAnswers, len(questions))
 }
